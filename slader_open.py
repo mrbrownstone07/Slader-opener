a3287a93998567e3f865084af24b0f53f04f0ec5
mrbrownstone07 <mahdi.mad07@gmail.com>
2019-11-30 11:01:07 +0600

    some correction done. '

--- slader_open.py
+++ slader_open.py
@@ -3,7 +3,7 @@ def slader():
     page_number = "asd"
     while page_number.lower()[0] != "q" or page_number.lower() !="quit":
         page_number = input("Page number: ")
-        question_number = input("Questionnumber: ")
+        question_number = input("Problem number: ")
         slader = "https://www.slader.com/textbook/9781118230718-fundamentals-of-physics-10th-edition/{}/exercises/{}/".format(page_number,question_number)
         webbrowser.open(slader)
 
